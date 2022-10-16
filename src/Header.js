ac9046d8858663e45bec78c9423938072e97cf28
jabongg <jbpvns@gmail.com>
2022-10-16 20:08:47 +0530

    first commit

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import {Button} from "reactstrap"
+
+function Header() {
+    return(    
+    <div style={{background:"#88c487"}}>
+        <h1>My Header</h1>
+        <Button>Click Me </Button>
+    </div>);
+}
+
+export default Header;
